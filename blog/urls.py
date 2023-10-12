cb1c744e7a556b7cb06b209c7d6eebe89aa4e2df
kimh7537 <kimh7537@gmail.com>
2023-10-12 16:01:35 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # path('<int:pk>/', views.single_post_page),     #pk 값을 받아 함수에 넘김
     # path('', views.index),
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
