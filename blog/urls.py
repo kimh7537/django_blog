3dccda5aecdf2fad2fb2504d18607254a90ba44a
kimh7537 <kimh7537@gmail.com>
2023-10-15 01:49:49 +0900

    로그인한 사용자에게만 작성페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # path('<int:pk>/', views.single_post_page),     #pk 값을 받아 함수에 넘김
     # path('', views.index),
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
